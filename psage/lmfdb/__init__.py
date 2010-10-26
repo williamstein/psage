0a5be08adc80f1daab9f2537978b69d84df68182
William Stein <wstein@gmail.com>
2010-10-26 15:41:39 -0700

    GPL3 everything -- I added a GPL3 header to the beginning of all the files I wrote...

--- psage/lmfdb/__init__.py
+++ psage/lmfdb/__init__.py
@@ -1,3 +1,25 @@
+#################################################################################
+#
+# (c) Copyright 2010 William Stein
+#
+#  This file is part of PSAGE
+#
+#  PSAGE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  PSAGE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+
 from mfdb import MFDB
 
 def reload():

5ec7f43ebe5220b59bfaf7d1c8d3bf2ce55f86be
William Stein <wstein@gmail.com>
2011-08-24 23:18:04 -0700

    Add a todo list to euler product L-function file.

--- psage/lseries/eulerprod.py
+++ psage/lseries/eulerprod.py
@@ -1,3 +1,41 @@
+"""
+General L-series
+
+AUTHOR:
+    - William Stein
+
+TODO:
+    - Symmetric powers (and modular degree -- see trac 9758)
+    - Triple product L-functions: Gross-Kudla, Zhang, etc -- see the code in triple_prod/triple.py
+    - Support L-calc L-function
+    - Make it so we use exactly one GP session for *all* of the Dokchitser L-functions
+    - Tensor products
+    - Genus 2 curves, via smalljac and genus2reduction
+    - Fast L-series of elliptic curves over number fields (not just sqrt(5)), via smalljac
+    - Inverse of number_of_coefficients function.
+"""
+
+#################################################################################
+#
+# (c) Copyright 2011 William Stein
+#
+#  This file is part of PSAGE
+#
+#  PSAGE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  PSAGE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
 import copy, math, types
 
 from sage.all import prime_range, cached_method, sqrt, SR, vector

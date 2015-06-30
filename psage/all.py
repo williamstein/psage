1cd8a4e5752f62f0ec3e5ab11989b69fe7b23dbe
Fredrik Strömberg <fredrik314@gmail.com>
2015-06-30 17:02:55 +0100

    fixed imports

--- psage/all.py
+++ psage/all.py
@@ -10,7 +10,7 @@
 #from ellff import ellff_EllipticCurve
 ### Then import everything we think the end user will want if he imports all from psage
 
-#from psage.groups.all import *
+from psage.groups.all import *
 from psage.modules.all import *
 from psage.modform.all import *
 from zfunctions import SelbergZeta,TransferOperator

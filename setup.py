d3115dc998e69f4f5771a049c075e69c043df546
Fredrik Stromberg <fredrik314@gmail.com>
2018-03-06 16:46:44 +0000

    updated include path

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ else:
 from module_list import ext_modules,aliases
 
 include_dirs = sage_include_directories(use_sources=True)
+include_dirs = include_dirs + [SAGE_LIB]
 include_dirs = include_dirs + [os.path.join(SAGE_LIB,"cysignals")]
 include_dirs = include_dirs + [os.path.join(SAGE_LIB,"sage/ext/")]
 
@@ -108,7 +109,7 @@ def run_cythonize():
     # enclosing Python scope (e.g. to perform variable injection).
     Cython.Compiler.Options.old_style_globals = True
     Cython.Compiler.Main.default_options['cache'] = False
-
+    print "include_dirs1=",include_dirs
     global ext_modules
     ext_modules = cythonize(
         ext_modules,

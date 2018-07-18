f8d3f3ebebe10b0ff1dbef3670aa56beb160f7f4
Fredrik Stromberg <fredrik314@gmail.com>
2018-07-18 11:51:31 +0200

    added include

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ from module_list import ext_modules,aliases
 
 include_dirs = sage_include_directories(use_sources=True)
 include_dirs = include_dirs + [SAGE_LIB]
-include_dirs = include_dirs + [os.path.join(SAGE_LIB,"cysignals")]
+include_dirs = include_dirs + [os.path.join(SAGE_LIB,"cysignals/")]
 include_dirs = include_dirs + [os.path.join(SAGE_LIB,"sage/ext/")]
-
+include_dirs = include_dirs + [os.path.join(SAGE_LIB,"sage/cpython/")]
 extra_compile_args = [ "-fno-strict-aliasing" ]
 extra_link_args = [ ]
 
@@ -109,7 +109,7 @@ def run_cythonize():
     # enclosing Python scope (e.g. to perform variable injection).
     Cython.Compiler.Options.old_style_globals = True
     Cython.Compiler.Main.default_options['cache'] = False
-    #print "include_dirs1=",include_dirs
+    print "include_dirs1=",include_dirs
     global ext_modules
     ext_modules = cythonize(
         ext_modules,
@@ -119,6 +119,7 @@ def run_cythonize():
         force=FORCE,
         include_path = include_dirs,
         aliases=aliases,
+        #exclude_failures=True,
         compiler_directives={
             'embedsignature': True,
             'profile': profile,

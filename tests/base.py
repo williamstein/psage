f22769eb3dc6571952226027b323ce06c2af28d1
Fredrik Stromberg <fredrik314@gmail.com>
2019-07-08 10:05:59 +0100

    added beginning of base class for unittests

---
+++ tests/base.py
@@ -0,0 +1,13 @@
+"""
+
+Base class for unittests (in case we need some particular functionality)
+
+"""
+import unittest
+
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+

1ea8f5c29aa2f9e7872c06854651bced993f5d27
Fredrik Stromberg <fredrik314@gmail.com>
2019-07-08 10:03:56 +0100

    changed logging format

--- psage/modform/maass/__init__.py
+++ psage/modform/maass/__init__.py
@@ -5,6 +5,6 @@ import os
 logging.basicConfig(
         level=int(os.getenv('LOG_LEVEL',40)),
         format='%(asctime)s %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]',
+        '[in %(filename)s:%(lineno)d]',
         datefmt='%Y%m%d-%H:%M%p',
         )
